63e6452ce664391948b3690a07b8dbb36129fef2
shannon wafula simiyu <81977577+shansim@users.noreply.github.com>
2023-02-08 02:45:05 +0300

    Dart functions task

---
+++ functions.dart
@@ -0,0 +1,29 @@
+// Task 1:
+int addTwo(int num1, int num2) {
+  return num1 + num2;
+}
+
+// Task 2:
+int subtractTwo(int num1, int num2) {
+  return num1 - num2;
+}
+
+// Task 3:
+int multiplyTwo(int num1, int num2) {
+  return num1 * num2;
+}
+
+// Task 4:
+int divideTwo(int num1, int num2) {
+  return num1 ~/ num2;
+}
+
+// Task 5:
+int stringLength(String str) {
+  return str.length;
+}
+
+// Task 6:
+dynamic getFirstElement(List list) {
+  return list.first;
+}
